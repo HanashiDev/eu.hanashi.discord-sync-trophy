cc67ad7448c5a066ed6deb7405e1d360a9a3a92b
Peter Lohse <hanashi@hanashi.eu>
2023-10-19 13:52:14 +0200

    6.0 Anpassung

--- make.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-del eu.hanashi.discord-sync-trophy.tar
-7z a -ttar -mx=9 eu.hanashi.discord-sync-trophy.tar .\* -x!eu.hanashi.discord-sync-trophy.tar -x!.git -x!.gitignore -x!make.bat
--- make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -f eu.hanashi.discord-sync-trophy.tar
-7z a -ttar -mx=9 eu.hanashi.discord-sync-trophy.tar ./* -x!eu.hanashi.discord-sync-trophy.tar -x!.git -x!.gitignore -x!make.bat
